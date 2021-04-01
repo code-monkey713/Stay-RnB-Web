dd0a7d9a8526a21d2ba30d57a25a7188c98bde6f
Tony Tran <codemonkey713@gmail.com>
2021-03-31 23:50:37 -0500

    adding barebone framework for expo

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Stay RnB!!!
+        You will get all your room stay and needs here!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
