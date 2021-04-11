e6f5b5d4f5632b9814647586d6ab46b98a5c7433
Joseph Soria <josephsoria6@gmail.com>
2021-04-11 11:39:53 -0500

    Wrote post request for new user to test with Insomnia

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require(`express`);
 const logger = require(`morgan`);
 const mongoose = require(`mongoose`);
 const path = require(`path`);
+const bcrypt = require(`bcrypt`);
 
-const PORT = 3000;
+const PORT = 3001;
 
 const db = require(`./models`);
 
@@ -15,6 +16,18 @@ app.use(express.json());
 app.use(express.static(`public`));
 
 mongoose.connect(`mongodb://localhost/Stay_RnB`, { useNewUrlParser: true });
+
+app.post(`/api/users`, async (req,res) => {
+    try {
+        req.body.password = await bcrypt.hash(req.body.password,10);
+        const newUser = await db.User.create(req.body);
+        res.status(200).json(newUser);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}.`)
