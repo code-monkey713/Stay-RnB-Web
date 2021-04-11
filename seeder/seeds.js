7a31d67419ecc07a4eb076030afde2661135c774
Joseph Soria <josephsoria6@gmail.com>
2021-04-10 22:25:21 -0500

    Added data for Owner seed

--- seeder/seeds.js
+++ seeder/seeds.js
@@ -8,7 +8,9 @@ mongoose.connect(`mongodb://localhost/Stay_RnB`, {
 
 const UserSeed = [
     {
-        username: `joey`,
+        username: `jsoria`,
+        firstName: `Joseph`,
+        lastName: `Soria`,
         password: `password`,
         email: `email@email.com`,
         userCreated: Date.now(),
@@ -16,24 +18,65 @@ const UserSeed = [
         isOwner: false,
     },
     {
-        username: `foo`,
+        username: `foobar`,
+        firstName: `foo`,
+        lastName: `bar`,
         password: `password`,
         email: `email@email2.com`,
         userCreated: Date.now(),
         isRenter: true,
     },
     {
-        username: `bar`,
+        username: `barfoo`,
+        firstName: `bar`,
+        lastName: `foo`,
         password: `password`,
         email: `email@email3.com`,
         userCreated: Date.now(),
         isOwner: true,
+        owner: 1,
     },
 ];
 
+const OwnerSeed = [
+    {
+        numberOfListings: 2,
+        listings: [
+            {
+                name: `First Listing`,
+                type: `Home`,
+                zip: 77095,
+                rooms: 3,
+                bathrooms: 1,
+                pets: `No`,
+                smoking: `No`,
+            },
+            {
+                name: `Second Listing`,
+                type: `Apartment`,
+                zip: 77084,
+                rooms: 2,
+                bathrooms: 1,
+                pets: `No`,
+                smoking: `Yes`,
+            },
+        ],
+        userId: 1,
+    },
+];
+
+
 db.User.deleteMany({})
 .then(() => db.User.collection.insertMany(UserSeed))
-.then(console.log(`Seed complete!`))
+.then(console.log(`User seed complete!`))
+.catch(err => {
+    console.log(err);
+    process.exit(1);
+});
+
+db.Owner.deleteMany({})
+.then(() => db.Owner.collection.insertMany(OwnerSeed))
+.then(console.log(`Owner seed complete!`))
 .catch(err => {
     console.log(err);
     process.exit(1);
