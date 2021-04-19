aacb64e9d1646dd4972e13555a165567eda7f819
Joseph Soria <josephsoria6@gmail.com>
2021-04-18 20:46:02 -0500

    Added login route

--- server.js
+++ server.js
@@ -26,41 +26,76 @@ app.use(express.static(`public`));
 mongoose.connect(`mongodb://localhost/Stay_RnB`, { useNewUrlParser: true });
 
 // Create new user
-app.post(`/api/users`, async (req,res) => {
+app.post(`/api/users`, async (req, res) => {
     try {
-        req.body.password = await bcrypt.hash(req.body.password,10);
+        req.body.password = await bcrypt.hash(req.body.password, 10);
         const newUser = await db.User.create(req.body);
         console.log(newUser)
         req.session.userId = newUser._id
         res.status(200).json(newUser);
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 });
 
 // Create new listing
-app.post(`/api/listings`, async ({body},res) => {
+app.post(`/api/listings`, async ({ body }, res) => {
     try {
         const newListing = await db.Owner.create(body);
         res.status(200).json(newListing);
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 });
 
 // Get user with listings
-app.get(`/api/users`, async (req,res) => {
+app.get(`/api/users`, async (req, res) => {
     try {
-        const getUser = await db.User.findOne({_id: req.session.userId}).populate(`listings`);
+        const getUser = await db.User.findOne({ _id: req.session.userId }).populate(`listings`);
         console.log(getUser)
         res.status(200).json(getUser);
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 })
+
+// Logging in
+app.post('/api/login', async (req, res) => {
+    try {
+        const userData = await db.User.findOne({email: req.body.email}).select(`password email username`);
+        console.log(userData)
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+        bcrypt.compare(req.body.password, userData.password, (err, isMatching) => {
+            if (err) {
+                console.log(err);
+            } else if (isMatching) {
+                req.session.userId = userData._id;
+                req.session.save(() => {
+                    req.session.loggedIn = true;
+                    res.json({ user: userData, message: 'You are now logged in!' });
+                });
+                console.log(`successful login!`)
+            } else {
+                res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+            }
+        })
+
+    } catch (err) {
+        res.status(400).json(err);
+    };
+});
+
 
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}.`)
