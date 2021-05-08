69d6fb133a61c5134a73a2428c245b7d6f6df388
Joseph Soria <josephsoria6@gmail.com>
2021-05-08 12:09:09 -0500

    modified seed functions to properly exit process after seed

--- seeder/seeds.js
+++ seeder/seeds.js
@@ -2,50 +2,50 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/Stay_RnB`, {
-    useNewUrlParser: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 const UserSeed = [
-    {
-        username: "jsoria",
-        firstName: "Joseph",
-        lastName: "Soria",
-        password: "password",
-        email: "email@email.com",
-        userCreated: Date.now(),
-        isRenter: false,
-        isOwner: false,
-    },
-    {
-        username: "foobar",
-        firstName: "foo",
-        lastName: "bar",
-        password: "password",
-        email: "email@email2.com",
-        userCreated: Date.now(),
-        isRenter: true,
-    },
-    {
-        username: "barfoo",
-        firstName: "bar",
-        lastName: "foo",
-        password: "password",
-        email: "email@email3.com",
-        userCreated: Date.now(),
-        isOwner: true,
-        owner: 1,
-    },
-    {
-        username: "randy",
-        firstName: "Random",
-        lastName: "Guy",
-        password: "password",
-        email: "email@email4.com",
-        userCreated: Date.now(),
-        isOwner: true,
-        owner: 2,
-    }
+  {
+    username: "jsoria",
+    firstName: "Joseph",
+    lastName: "Soria",
+    password: "password",
+    email: "email@email.com",
+    userCreated: Date.now(),
+    isRenter: false,
+    isOwner: false,
+  },
+  {
+    username: "foobar",
+    firstName: "foo",
+    lastName: "bar",
+    password: "password",
+    email: "email@email2.com",
+    userCreated: Date.now(),
+    isRenter: true,
+  },
+  {
+    username: "barfoo",
+    firstName: "bar",
+    lastName: "foo",
+    password: "password",
+    email: "email@email3.com",
+    userCreated: Date.now(),
+    isOwner: true,
+    owner: 1,
+  },
+  {
+    username: "randy",
+    firstName: "Random",
+    lastName: "Guy",
+    password: "password",
+    email: "email@email4.com",
+    userCreated: Date.now(),
+    isOwner: true,
+    owner: 2,
+  }
 ];
 
 const ListingSeed = [
@@ -71,26 +71,42 @@ const ReviewSeed = [
   }
 ]
 
-db.User.deleteMany({})
-.then(() => db.User.collection.insertMany(UserSeed))
-.then(console.log("User seed complete!"))
-.catch(err => {
-    console.log(err);
-    process.exit(1);
-});
+function seedUsers() {
+  db.User.deleteMany({})
+    .then(() => db.User.collection.insertMany(UserSeed))
+    .then(console.log("User seed complete!"))
+    .catch(err => {
+      console.log(err);
+      process.exit(1);
+    });
+}
 
-db.Listings.deleteMany({})
-.then(() => db.Listings.collection.insertMany(ListingSeed))
-.then(console.log("Listing seed complete!"))
-.catch(err => {
-    console.log(err);
-    process.exit(1);
-});
+function seedListings() {
+  db.Listings.deleteMany({})
+    .then(() => db.Listings.collection.insertMany(ListingSeed))
+    .then(console.log("Listing seed complete!"))
+    .catch(err => {
+      console.log(err);
+      process.exit(1);
+    });
+}
 
-db.Reviews.deleteMany({})
-.then(() => db.Reviews.collection.insertMany(ReviewSeed))
-.then(console.log("Reviews seed complete!"))
-.catch(err => {
-    console.log(err);
-    process.exit(1);
-});
+function seedReviews() {
+  db.Reviews.deleteMany({})
+    .then(() => db.Reviews.collection.insertMany(ReviewSeed))
+    .then(console.log("Reviews seed complete!"))
+    .catch(err => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+async function seedAll() {
+  await seedUsers();
+  await seedListings();
+  await seedReviews();
+
+  process.exit(0);
+}
+
+seedAll();
