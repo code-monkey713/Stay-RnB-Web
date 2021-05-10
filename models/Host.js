2dd1d0d0870089a685978f3fc82ed0d2cf4d9bc1
Tony Tran <codemonkey713@gmail.com>
2021-05-09 23:33:45 -0500

    added host model; updated index models; updated user model to fit with change in model for user and host; updated seed data for user to meet requirement for model structure

--- models/Host.js
+++ models/Host.js
@@ -1,19 +1,49 @@
 const mongoose = require(`mongoose`);
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const HostSchema = new Schema({
-    numberOfListings: {
-        type: Number,
+  username: {
+    type: String,
+    trim: true,
+    required: [true, `Please enter a valid username.`],
+    unique: [true, `This username is already taken.`],
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: [true, `Please enter a valid password`],
+    minlength: [8, `Password must be at least 8 characters.`],
+    select: false,
+  },
+  firstName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: [true, `This email is already taken`],
+    required: [true, `Please enter a valid e-mail address`],
+    match: [/.+@.+\..+/, `Please enter a valid e-mail address`],
+  },
+  userCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+  numberOfListings: {
+    type: Number,
+  },
+  listings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Listings',
     },
-    listings: {
-        type: Array,
-    },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
+  ],
 });
 
-const Host = mongoose.model(`Host`,HostSchema);
+const Host = mongoose.model(`Host`, HostSchema);
 
 module.exports = Host;
