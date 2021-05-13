4d2287f762a8c0c0dcf2b5deb329b3c8867922a5
Joseph Soria <josephsoria6@gmail.com>
2021-05-12 19:36:31 -0500

    imported Link, added links to nav tabs

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ const App = () => {
         </Routes>
       </div>
       <NavTabs />
-
     </BrowserRouter>
   );
 };
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export const NavTabs = () => {
   return (
     <div className="footer row m-auto">
-        <div className="col tab">
-          <a href="#" className="tabLinks fs-2">Login</a>
-        </div>
-        <div className="col tab">
-          <a href="#" className="tabLinks fs-2">Sign Up</a>
-        </div>
-        <div className="col tab">
-          <a href="#" className="tabLinks fs-2">Listings</a>
-        </div>
-        <div className="col tab">
-          <a href="#" className="tabLinks fs-2">Profile</a>
+      <div className="col tab">
+        <li className="tabList fs-2">
+          <Link className="tabLink"  to="/login">Login</Link>
+        </li>
+      </div>
+      <div className="col tab">
+        <li className="tabList fs-2">
+          <Link className="tabLink"  to="/signup">Sign Up</Link>
+        </li>
+      </div>
+      <div className="col tab">
+        <li className="tabList fs-2">
+          <Link className="tabLink" to="/listing">Listings</Link>
+        </li>
+      </div>
+      <div className="col tab">
+        <li className="tabList fs-2">
+          <Link className="tabLink" to="/profile">Profile</Link>
+        </li>
       </div>
     </div>
   );
