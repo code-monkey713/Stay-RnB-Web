5401d2e73be04902c119808510ad751a2e6af8a3
Joseph Soria <josephsoria6@gmail.com>
2021-05-16 21:26:57 -0500

    made UserProfile component

---
+++ src/components/Profiles/UserProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const UserProfile = () => {
+  return (
+    <div>
+      <h1 className="text-center">User Profile</h1>
+    </div>
+  );
+};
+
+export default UserProfile;
