3f0a3396d82a186b651bcaa3b13a11922d69fe8a
Joseph Soria <josephsoria6@gmail.com>
2021-05-19 17:09:44 -0500

    installed cors for express and set config in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require(`morgan`);
 const mongoose = require(`mongoose`);
 const PORT = process.env.PORT || 3001;
 const routes = require(`./controllers`);
+const cors = require('cors');
 
 const app = express();
 
@@ -20,12 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`public`));
 app.use(routes);
+app.use(cors());
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/Stay_RnB`, { useNewUrlParser: true });
 
