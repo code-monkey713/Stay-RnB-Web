9fa4b6d4a7202cf7144aa3bb96c93e2cc8f5e16b
Joseph Soria <josephsoria6@gmail.com>
2021-05-20 18:08:39 -0500

    made function to post listing, login, and logout

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,18 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
+const herokuUri = "https://stay-rnb-server.herokuapp.com";
+const localAPIServer = "http://localhost:3001";
 
-export default {
+const API = {
   // get all listings
   getListings: () => {
-    return axios.get('/api/listings');
+    return axios.get(`${localAPIServer}/api/listings`);
+  },
+
+  postListing: () => {
+    return axios.post(`${herokuUri}/api/listings`);
   },
 
   // delete a listing by ID
   deleteListing: (id) => {
-    return axios.delete('/:id' + id);
+    return axios.delete("/:id" + id);
   },
 
   // creates new user
   createUser: (userData) => {
-    return axios.post('/api/users', userData);
+    return axios.post(`${localAPIServer}/api/users`, {data: userData});
   },
+
+  login: (userData) => {
+    return axios.post(`${localAPIServer}/api/users/login`, {data: userData});
+  },
+
+  logout: () => {
+    return axios.post(`${localAPIServer}/api/users/logout`);
+  }
 };
+
+export default API;
