d2818e7a90d7b6c4a547c8d0915f9781a96b5cd8
Tony Tran <codemonkey713@gmail.com>
2021-05-20 20:32:40 -0500

    added working login authentication page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,153 +1,58 @@
-import { useState, useRef, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import API from "../utils/API";
-import env from 'react-dotenv';
-import AuthContext from '../store/auth-context';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { Link, useHistory } from 'react-router-dom';
 
 export const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const authCtx = useContext(AuthContext);
+  const { login } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
-  const [isLogin, setIsLogin] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isLogged, setIsLogged]= useState();
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-  const submitHandler = (event) => {
-    event.preventDefault();
+    try {
+      setError('');
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push('/');
+    } catch {
+      setError('Failed to log in');
+    }
 
-    const enteredEmail = emailRef.current.value;
-    const enteredPassword = passwordRef.current.value;
-
-    // add validation on input
-
-    API.userLogin({
-      email: enteredEmail,
-      password: enteredPassword,
-    }).then((res) => {
-      console.log('Logged In Okay');
-      setIsLogged(res);
-      console.log(isLogged);
-      
-    });
-
-    // setIsLoading(true);
-    // let url;
-    // if (isLogin) {
-    //   url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.API_KEY}`;
-    // } else {
-    //   url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.API_KEY}`;
-    // }
-    // fetch(url, {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     email: enteredEmail,
-    //     password: enteredPassword,
-    //     returnSecureToken: true,
-    //   }),
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // })
-    //   .then((res) => {
-    //     setIsLoading(false);
-    //     console.log(res);
-    //     if (res.ok) {
-    //       return res.json();
-    //     } else {
-    //       return res.json().then((data) => {
-    //         let errorMessage = 'Authentication failed!';
-    //         // if (data && data.error && data.error.message) {
-    //         //   errorMessage = data.error.message;
-    //         // }
-    //         throw new Error(errorMessage);
-    //       });
-    //     }
-    //   })
-    //   .then((data) => {
-    //     const expirationTime = new Date(
-    //       new Date().getTime() + +data.expiresIn * 1000
-    //     );
-    //     authCtx.login(data.idToken, expirationTime.toISOString());
-    //     // history.replace('/');
-    //   })
-    //   .catch((err) => {
-    //     alert(err.message);
-    //   });
-
-    // console.log(`Email: ${enteredEmail} | Password: ${enteredPassword}`);
-  };
+    setLoading(false);
+  }
 
   return (
-    <div>
-      <form onSubmit={submitHandler}>
-        <h2 className="text-center mt-5 mb-3">Already a user? Sign In!</h2>
-
-        <div className="form-group row mb-3">
-          <div className="col-6 m-auto">
-            <label className="fs-4">Email address</label>
-            <input
-              type="email"
-              id="email"
-              required
-              ref={emailRef}
-              className="form-control"
-              placeholder="Enter email"
-            />
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Log In
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/recover">Forgot Password?</Link>
           </div>
-        </div>
-
-        <div className="form-group row mb-3">
-          <div className="col-6 m-auto">
-            <label className="fs-4">Password</label>
-            <input
-              type="password"
-              id="password"
-              required
-              ref={passwordRef}
-              className="form-control"
-              placeholder="Enter password"
-            />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col form-check d-flex justify-content-center">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              value=""
-              id="rememberCheck"
-            />
-            <label htmlFor="rememberCheck" className="mx-2 mb-2 fs-6">
-              Remember me
-            </label>
-          </div>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col d-flex justify-content-center">
-            <button type="submit" className="btn btn-primary btn-block">
-              Submit
-            </button>
-          </div>
-        </div>
-      </form>
-
-      <div className="row">
-        <div className="col d-flex justify-content-center">
-          <p className="forgot-password fs-5">
-            Forgot <Link to="/recover"> password?</Link>
-          </p>
-        </div>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
-      <div className="row">
-        <div className="col d-flex justify-content-center">
-          <p className="forgot-password fs-5">
-            Not a user yet? Sign up <Link to="/signup">here.</Link>
-          </p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
