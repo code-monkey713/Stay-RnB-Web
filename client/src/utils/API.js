ef6db85c75007db4670415b5f66a267be92f7942
Joseph Soria <josephsoria6@gmail.com>
2021-05-21 15:35:31 -0500

    added function to get user per id

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const API = {
-  // get all listings
+  // get listings by zip
   getListings: (zip) => {
     return axios.get(`/api/listings/:zip`, { params: { zip: zip } });
   },
 
+  // create listing
   postListing: (listingData) => {
     return axios.post(`/api/listings`, { data: listingData });
   },
@@ -15,15 +16,22 @@ const API = {
     return axios.delete("/:id" + id);
   },
 
+  // gets user by id
+  getUser: () => {
+    return axios.get("/api/users");
+  },
+
   // creates new user
   createUser: (userData) => {
     return axios.post(`/api/users`, { data: userData });
   },
 
+  // logs user in
   login: (userData) => {
     return axios.post(`/api/users/login`, { data: userData });
   },
 
+  // logs user out
   logout: () => {
     return axios.post(`/api/users/logout`);
   },
