f4c58f2a4f7875ed092fd5238ba1dba77b5cdbb0
Tony Tran <codemonkey713@gmail.com>
2021-05-20 21:31:14 -0500

    updated changes to signup page to connect and save to firebase

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,122 +1,64 @@
-import React, { useRef } from "react";
-import { Link } from "react-router-dom";
-import API from "../utils/API";
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { Link, useHistory } from 'react-router-dom';
 
-export const Signup = () => {
-  const username = useRef("");
-  const firstName = useRef("");
-  const lastName = useRef("");
-  const emailAddress = useRef("");
-  const password = useRef("");
+export function Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
-  const submitFormHandler = (event) => {
-    event.preventDefault();
-    console.log("add validation for data entered");
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    API.createUser({
-      username: username.current.value,
-      password: password.current.value,
-      firstName: firstName.current.value,
-      lastName: lastName.current.value,
-      email: emailAddress.current.value,
-    }).then(() => {
-      alert("The user has been created successfully!");
-    });
-  };
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Passwords do not match');
+    }
+
+    try {
+      setError('');
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      history.push('/');
+    } catch {
+      setError('Failed to create an account');
+    }
+
+    setLoading(false);
+  }
 
   return (
-    <div>
-      <form onSubmit={submitFormHandler}>
-        <h3 className="text-center mt-5 mb-3">
-          New to Stay RnB? Please sign up:
-        </h3>
-
-        <div className="row mb-3">
-          <div className="col-6 m-auto">
-            <div className="form-group">
-              <label className="fs-4">Username </label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="What username would you like to use?"
-                ref={username}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col-6 m-auto">
-            <div className="form-group">
-              <label className="fs-4">First Name </label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="First Name"
-                ref={firstName}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col-6 m-auto">
-            <div className="form-group">
-              <label className="fs-4">Last name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Last Name"
-                ref={lastName}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col-6 m-auto">
-            <div className="form-group">
-              <label className="fs-4">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter Email"
-                ref={emailAddress}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col-6 m-auto">
-            <div className="form-group">
-              <label className="fs-4">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Enter Password"
-                ref={password}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col d-flex justify-content-center">
-            <button type="submit" className="btn btn-primary btn-block">
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
               Sign Up
-            </button>
-          </div>
-        </div>
-      </form>
-
-      <div className="row">
-        <div className="col d-flex justify-content-center">
-          <p className="forgot-password">
-            Already registered? Sign in<Link to="/login"> here.</Link>
-          </p>
-        </div>
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>
       </div>
-    </div>
+    </>
   );
-};
+}
