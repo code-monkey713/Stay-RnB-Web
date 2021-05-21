3a1129b63945aba295cba9d3772c87d0617bf7c7
Joseph Soria <josephsoria6@gmail.com>
2021-05-21 15:35:19 -0500

    added conditional rendering for on-page loading

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { HostProfile } from '../components/Profiles/';
-import { UserProfile } from '../components/Profiles/';
-
+import React, { useState, useEffect } from "react";
+import { HostProfile } from "../components/Profiles/";
+import { UserProfile } from "../components/Profiles/";
+import API from "../utils/API";
 
 export const Profile = () => {
-  return (
-    <div>
-      <HostProfile />
-      {/* <UserProfile /> */}
+  const [userInfo, setUserInfo] = useState({});
 
-    </div>
-  );
+  useEffect(async () => {
+    const { data } = await API.getUser();
+    setUserInfo(data);
+  }, []);
+
+  if (Object.entries(userInfo).length > 0) {
+    return (
+      <div>
+        <HostProfile userInfo={userInfo} />
+        {/* <UserProfile /> */}
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
-
