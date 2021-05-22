f1b217a5aa84d70503ea8d10d8d6d90a48094bc5
Joseph Soria <josephsoria6@gmail.com>
2021-05-21 19:15:04 -0500

    added conditional for host logging

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,10 +7,16 @@ router.post(`/`, async (req, res) => {
   try {
     req.body.data.password = await bcrypt.hash(req.body.data.password, 10);
     const newUser = await db.User.create(req.body.data);
-    await db.Host.create(req.body.data);
-    req.session.userId = newUser._id;
+    const newHost = await db.Host.create(req.body.data);
     req.session.loggedIn = true;
-    res.status(200).json({ user: newUser, message: "New user created" });
+    if (!req.body.data.hostLogin) {
+      req.session.userId = newUser._id;
+      res.status(200).json({ user: newUser, message: "New user created" });
+    } else {
+      req.session.userId = newHost._id;
+      console.log(req.session.userId)
+      res.status(200).json({ user: newHost, message: "New host created" });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -20,12 +26,12 @@ router.post(`/`, async (req, res) => {
 // Logging in
 router.post("/login", async (req, res) => {
   try {
-    if (req.body.data.host === false) {
+    if (!req.body.data.hostLogin) {
       const userData = await db.User.findOne({
         email: req.body.data.email,
       }).select(`password email username`);
       req.session.save(() => {
-        req.session.host = false;
+        req.session.hostLogin = false;
       });
       handleLogin(req, res, userData);
     } else {
@@ -33,7 +39,7 @@ router.post("/login", async (req, res) => {
         email: req.body.data.email,
       }).select(`password email username`);
       req.session.save(() => {
-        req.session.host = true;
+        req.session.hostLogin = true;
       });
       handleLogin(req, res, userData);
     }
@@ -42,33 +48,6 @@ router.post("/login", async (req, res) => {
     res.status(400).json(err);
   }
 });
-
-// Logging out
-router.post("/logout", (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-
-// Get user by id
-router.get("/", async (req,res) => {
-  try {
-    if (req.session.host === false) {
-      const getUser = await db.User.findById(req.session.userId);
-      res.status(200).json(getUser);
-    } else {
-      const getUser = await db.Host.findById(req.session.userId).populate('listings');
-      res.status(200).json(getUser);
-    }
-  } catch(err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-})
 
 const handleLogin = (req, res, userData) => {
   if (!userData) {
@@ -89,7 +68,6 @@ const handleLogin = (req, res, userData) => {
           req.session.loggedIn = true;
           res.json({ user: userData, message: "You are now logged in!" });
         });
-        console.log(`successful login!`);
       } else {
         res
           .status(400)
@@ -99,5 +77,37 @@ const handleLogin = (req, res, userData) => {
     }
   );
 };
+
+// Logging out
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+// Get user by id
+router.get("/", async (req, res) => {
+  try {
+    if (!req.session.hostLogin) {
+      const getUser = await db.User.findById(req.session.userId);
+      res.status(200).json(getUser);
+    } else {
+      const getUser = await db.Host.findById(req.session.userId).populate(
+        "listings"
+      );
+      res.status(200).json(getUser);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
 
 module.exports = router;
