5de87516fbb6b9ee8dca0a31c0ab1d51e4077d18
Joseph Soria <josephsoria6@gmail.com>
2021-05-21 19:11:03 -0500

    added props to user profile

--- client/src/components/Profiles/UserProfile.js
+++ client/src/components/Profiles/UserProfile.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Woman from "../../assets/Woman.jpg";
-import { ListingCard } from "../../components";
+import Man from "../../assets/Man.jpg";
 
-export const UserProfile = () => {
+export const UserProfile = ({ userInfo }) => {
   return (
     <>
       <div className="row profileContainer">
         <div className="col d-flex justify-content-center align-items-center">
-          <img src={Woman} className="img-fluid userImage" />
+          <img src={Man} className="img-fluid userImage" />
         </div>
         <div className="col">
-          <h1>User Profile</h1>
-          <p className="fs-5">First Name: Bar</p>
-          <p className="fs-5">Last Name: Foo</p>
-          <p className="fs-5">Email Address: BarFoo@email.com</p>
+          <p className="fs-5 text-capitalize">First Name: {userInfo.firstName}</p>
+          <p className="fs-5 text-capitalize">Last Name: {userInfo.lastName}</p>
+          <p className="fs-5">Email Address: {userInfo.email}</p>
           <p className="fs-5">Phone Number: 098-765-4321</p>
-          <p className="fs-5">Address: 789 Random Woman Ln, Houston, Tx 77777</p>
+          <p className="fs-5">
+            Address: 789 Random Woman Ln, Houston, Tx 77777
+          </p>
           <p className="fs-5">Areas of Interest: 77777, 12345</p>
         </div>
       </div>
